902eeae652ce32cfbf836c3106f892ce1bc48a59
Romartiny <romartiny@Romartiny-2.local>
2023-01-16 17:52:51 +0300

    complete front-end part

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,10 +3,35 @@ const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
     modeToggle = body.querySelector('.mode-toggle');
 
+let getMode = localStorage.getItem('mode');
+let getStatus = localStorage.getItem('status');
+
+if(getMode && getMode === 'dark') {
+    body.classList.toggle('dark');
+} else {
+    body.classList.toggle('light');
+}
+
+if(getStatus && getStatus === 'close') {
+    sidebar.classList.toggle('close');
+} else {
+    sidebar.classList.toggle('open');
+}
+
 modeToggle.addEventListener('click', () => {
     body.classList.toggle('dark');
+    if(body.classList.contains('dark')) {
+        localStorage.setItem('mode', 'dark');
+    } else {
+        localStorage.setItem('mode', 'light');
+    }
 });
 
 sidebarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('close');
+    if(sidebar.classList.contains('close')) {
+        localStorage.setItem('status', 'close');
+    } else {
+        localStorage.setItem('status', 'open');
+    }
 })
