784b943c9c53f0ddab698f26323e650fabb2f69c
Romartiny <romartiny@Romartiny-2.local>
2023-02-22 11:36:24 +0300

    fixing bugs + fix bridge point

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,17 @@
+@extends('shopify-app::layouts.default')
+
+@section('content')
+    <!-- You are: (shop domain name) -->
+    <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+@endsection
+
+@section('scripts')
+    @parent
+
+    <script>
+        actions.TitleBar.create(app, { title: 'Hiiiii' });
+    </script>
+@endsection
 <html lang="en">
 
 <head>
--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,11 @@ use App\Http\Controllers\InfoController;
 use App\Http\Controllers\HelpController;
 
 Route::middleware(['verify.shopify'])->group(function () {
-    Route::get('/', [DashboardController::class, 'showDashboard'])
-        ->name('admin-events');
-    Route::get('/admin-events', [AdminEventsController::class, 'showAdminEvents'])
-        ->name('admin-events');
-    Route::get('/order-events', [OrderEventsController::class, 'showOrderEvents'])
-        ->name('order-events');
-    Route::get('/product-events', [ProductEventsController::class, 'showProductEvents'])
-        ->name('product-events');
-    Route::get('/transaction-events', [TransactionEventsController::class, 'showTransactionEvents'])
-        ->name('transaction-events');
-    Route::get('/info', [InfoController::class, 'showShopInfo'])
-        ->name('shop-information');
-    Route::get('/help', [HelpController::class, 'showAppInfo'])
-        ->name('help-information');
+    Route::get('/', [DashboardController::class, 'showDashboard'])->name('home')->middleware(['verify.shopify']);
+    Route::get('/admin-events', [AdminEventsController::class, 'showAdminEvents'])->name('admin-events')->middleware(['verify.shopify']);
+    Route::get('/order-events', [OrderEventsController::class, 'showOrderEvents'])->name('order-events')->middleware(['verify.shopify']);
+    Route::get('/product-events', [ProductEventsController::class, 'showProductEvents'])->name('product-events')->middleware(['verify.shopify']);
+    Route::get('/transaction-events', [TransactionEventsController::class, 'showTransactionEvents'])->name('transaction-events')->middleware(['verify.shopify']);
+    Route::get('/info', [InfoController::class, 'showShopInfo'])->name('shop-information')->middleware(['verify.shopify']);
+    Route::get('/help', [HelpController::class, 'showAppInfo'])->name('help-information')->middleware(['verify.shopify']);
 });
