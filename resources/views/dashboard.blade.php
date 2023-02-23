93407f9b540b692fb61daf42f331a3eda97deeb2
Romartiny <romortiny@gmail.com>
2023-02-23 12:44:08 +0300

    create layouts for admin events page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
@@ -12,7 +12,9 @@
 </head>
 
 <body>
-    @extends('layouts.loader')
-    @extends('layouts.admin.dashboardBlock')
-    @extends('layouts.nav')
+
+@extends('layouts.loader')
+@extends('layouts.dashboard.dashboardBlock')
+@extends('layouts.nav')
+
 </body>
