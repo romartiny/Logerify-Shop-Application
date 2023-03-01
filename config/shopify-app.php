dc949cc74d439adf423f1f0c78ad9fdbceb614dc
Romartiny <romortiny@gmail.com>
2023-03-02 00:22:41 +0300

    update .env and config shopify(move some data)

--- config/shopify-app.php
+++ config/shopify-app.php
@@ -150,7 +150,7 @@ return [
     |
     */
 
-    'app_name' => env('SHOPIFY_APP_NAME', 'Shopify App'),
+    'app_name' => env('SHOPIFY_APP_NAME', ''),
 
     /*
     |--------------------------------------------------------------------------
@@ -164,7 +164,7 @@ return [
     |
     */
 
-    'api_version' => env('SHOPIFY_API_VERSION', 'SHOPIFY_API_VERSION'),
+    'api_version' => env('SHOPIFY_API_VERSION', ''),
 
     /*
     |--------------------------------------------------------------------------
@@ -205,7 +205,7 @@ return [
     read_products, read_product_listings, read_publications, read_purchase_options, read_reports,
     read_resource_feedbacks, read_script_tags, read_shipping, read_shopify_payments_disputes,
     read_shopify_payments_payouts, read_returns, read_themes, read_translations,
-    read_third_party_fulfillment_orders, read_order_edits, read_product_listings	'),
+    read_third_party_fulfillment_orders, read_order_edits, read_product_listings'),
 
     /*
     read_users - shopify plus
@@ -232,7 +232,7 @@ return [
     |
     */
 
-    'api_redirect' => env('SHOPIFY_API_REDIRECT', '/authenticate'),
+    'api_redirect' => env('SHOPIFY_API_REDIRECT', ''),
 
     /*
     |--------------------------------------------------------------------------
